7f6697bc9d56829c6408aba996b2a42e4b1ff33d
manishachappidi <user599@host.docker.internal>
2023-10-26 11:36:13 +0530

    My First Commit

---
+++ deveops-demo/src/main/java/com/example/demo/DeveopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveopsDemoApplication.class, args);
+	}
+
+}
